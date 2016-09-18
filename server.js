f58efcf113797582bdb40bd1cb4d9c5223c08069
Hariharan1988 <hvs.hari88@gmail.com>
2016-09-18 15:38:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/ ArticleOne',function(req,res){
+app.get('/ article-one',function(req,res){
 res.send ('Article one is requested and it is served here');
 });
-app.get('/ ArticleTwo',function(req,res){
+app.get('/ article-two',function(req,res){
 res.send('Article Two is Requested and it will be served here');
 });
-app.get('/Articlethree',function(req,res){
+app.get('/article-three',function(req,res){
 res.send('Article three is requested and it will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
