283276e5ce0b057343db32d652ba0a6b00e648ca
Hariharan.V <hvs.hari88@gmail.com>
2016-09-30 21:14:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
-// My animation in moving the picture Main_Text div
-var element=document.getElementById('Main_Text');
+//  Animation in moving the picture Main_Text div
+//var element=document.getElementById('Main_Text');
+var img=document.getElementById('Hari')
+
+img.onclick=function(){ img.style.marginLeft='100px';
+    
+};
