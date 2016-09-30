c04cfd894813d235bc248a1bcefb93cc429f038b
Hariharan.V <hvs.hari88@gmail.com>
2016-09-30 21:33:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,14 @@
 console.log('Loaded!');
 //  Animation in moving the picture Main_Text div
-//var element=document.getElementById('Main_Text');
+var element=document.getElementById('Main_Text');
 var img=document.getElementById('Hari');
+
 var marginLeft=0;
 
 function MoveRight ()
 {
  marginLeft=marginLeft+1;
+ 
  img.style.marginLeft=marginLeft+'px';
     
     
