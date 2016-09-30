a5d21009b08bf1abd7cc24dcac7840eba66ff5d6
Hariharan.V <hvs.hari88@gmail.com>
2016-09-30 21:37:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,23 @@
 console.log('Loaded!');
 //  Animation in moving the picture Main_Text div
 var element=document.getElementById('Main_Text');
+
 var img=document.getElementById('Hari');
 
 var marginLeft=0;
 
 function MoveRight ()
 {
- marginLeft=marginLeft+1;
+ marginLeft=marginLeft + 1;
  
- img.style.marginLeft=marginLeft+'px';
+ img.style.marginLeft=marginLeft + 'px';
     
     
 }
 
 img.onclick=function(){
 
-var interval=setinterval(MoveRight,50);
+var interval=setInterval (MoveRight , 50);
 
 };
 //img.onclick=function(){ img.style.marginLeft='100px';
