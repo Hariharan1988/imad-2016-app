3ee3c396347c1fac12f46a91a6ac93598595e8f2
Hariharan.V <hvs.hari88@gmail.com>
2016-10-23 16:53:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -242,7 +242,7 @@ app.get('/ui/Hariharan.png', function (req, res) {
 });
 
 
-var port = 8083; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8083, function () {
+var port = 80; // Use 8080 for local development because you might already have apache running on 80
+app.listen(80, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
