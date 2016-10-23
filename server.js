305df734e1e00d4545b70dc840727ba6df58c802
Hariharan.V <hvs.hari88@gmail.com>
2016-10-23 19:42:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -225,6 +225,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+});
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 //app.get('/:ProfileofHari', function (req,res){
