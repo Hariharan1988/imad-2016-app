ae55880243c0a6a287d7a7e7cfbfa0b1d4f7de26
Hariharan.V <hvs.hari88@gmail.com>
2016-10-26 07:44:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ return htmlTemplate;
 }
 
 
-app.get('/', function (req, res) {
+app.get('/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));// Registration Page
 });
 app.get('/login.html', function (req, res) {
